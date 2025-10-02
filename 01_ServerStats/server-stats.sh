6b0d0880848a66863f2d01b235613e612a86ece4
root <root@LAPTOP-1IF53HCU.localdomain>
2025-10-03 06:58:09 +0700

    feat(disk_usage): Add get_disk_usage

--- 01_ServerStats/server-stats.sh
+++ 01_ServerStats/server-stats.sh
@@ -10,9 +10,23 @@ get_memory_data(){
 	print "Free: " ($6/$4)*100"%"
 	print "Used: " ($8/$4)*100"%"}'
 }
-cpu=$(get_cpu_usage)
-mem=$(get_memory_data)
-echo "Total CPU Usage: $cpu%"
-echo "Memory data:"
-echo "$mem"
+get_disk_usage(){
+	total=0
+	for size in $(df -B1 | awk '(NR>1 && $2 ~ /^[0-9]+$/) {print $2}'); do
+		total=$((total +$size)); 
+	done;
+	numfmt --to=iec $total
+}
+main(){
+	cpu=$(get_cpu_usage)
+	mem=$(get_memory_data)
+	disk_usage=$(get_disk_usage)
+	echo "Total CPU Usage: $cpu%"
+	echo "Memory data:"
+	echo "$mem"
+	echo "Total disk usage: $disk_usage"
+}
 
+if [[ "${BASH_SOURCE[0]}" == "$0" ]]; then
+	main "$@"
+fi

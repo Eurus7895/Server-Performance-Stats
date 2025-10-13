a53f31e63147c42e127062247b80f84b13e71b28
Hoang Nguyen <t.hoang7895@gmail.com>
2025-10-13 23:05:53 +0000

    feat(server_stats): Add top 5 memory processes

--- 01_ServerStats/server-stats.sh
+++ 01_ServerStats/server-stats.sh
@@ -10,6 +10,7 @@ get_memory_data(){
 	print "Free: " ($6/$4)*100"%"
 	print "Used: " ($8/$4)*100"%"}'
 }
+
 get_disk_usage(){
 	total=0
 	for size in $(df -B1 | awk '(NR>1 && $2 ~ /^[0-9]+$/) {print $2}'); do
@@ -17,14 +18,31 @@ get_disk_usage(){
 	done;
 	numfmt --to=iec $total
 }
+
+get_top5_cpu_processes(){
+	ps -eo pid,user,pcpu,pmem,comm --sort=-pcpu | head -n 6 | awk '{print $3,$5}'		
+}
+
+get_top5_mem_processes(){
+	ps -eo pid,user,pcpu,pmem,comm --sort=-pmem | head -n 6 | awk '{printf "%s\t%s\n", $4, $5}'
+}
+
 main(){
 	cpu=$(get_cpu_usage)
 	mem=$(get_memory_data)
 	disk_usage=$(get_disk_usage)
+	top5_cpu_processes=$(get_top5_cpu_processes)
+	top5_mem_processes=$(get_top5_mem_processes)
+
 	echo "Total CPU Usage: $cpu%"
+	echo -e "\n"
 	echo "Memory data:"
 	echo "$mem"
+	echo -e "\n"
 	echo "Total disk usage: $disk_usage"
+	echo -e "\n"
+	echo -e "Top 5 CPU processes are: \n$top5_cpu_processes\n"
+	echo -e "Top 5 MEM processes are: \n$top5_mem_processes\n"
 }
 
 if [[ "${BASH_SOURCE[0]}" == "$0" ]]; then
